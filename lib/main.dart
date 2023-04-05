19a47a6f8747cd052bb55022768553acfaacc059
CUMT-Atom <cnatom@foxmail.com>
2023-04-05 23:46:37 +0800

    🐛bugfix(main.dart):SnackBar导致的红屏

--- lib/main.dart
+++ lib/main.dart
@@ -63,22 +63,11 @@ class _LoginPageState extends State<LoginPage> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    /// 停用并移植到新版本
-    if (DateTime.now().isAfter(DateTime(2023, 3, 25))) {
-      return const Scaffold(
-        body: Center(
-          child: Text(
-            "该测试版已过期\n请加QQ群：839372371或957634136获取最新版本",
-            textAlign: TextAlign.center,
-          ),
-        ),
-      );
-    }
     return GestureDetector(
       onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('矿小助CUMT校园网登录 1.3'),
+          title: const Text('矿小助CUMT校园网登录'),
         ),
         body: Center(
           child: Padding(
@@ -86,13 +75,6 @@ class _LoginPageState extends State<LoginPage> with WidgetsBindingObserver {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  "新功能：\n"
-                      "1.添加了文昌校区接口\n"
-                      "2.多账号功能\n（账号栏右边的小按钮，当成功登录后自动保存）\n"
-                      "自动登录时机：初始化App或从后台调出",
-                  style: TextStyle(color: Colors.grey),
-                ),
                 const SizedBox(height: 16.0),
                 buildTextField("账号", _usernameController, showPopButton: true),
                 const SizedBox(height: 16.0),
@@ -121,14 +103,6 @@ class _LoginPageState extends State<LoginPage> with WidgetsBindingObserver {
                 ),
                 const SizedBox(
                   height: 20,
-                ),
-                const Text(
-                  "本App用于测试矿小助新功能\n"
-                  "3月25日之后将停用\n"
-                  "如果遇到问题请加QQ群反馈\n"
-                  "1群：839372371，2群：957634136",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.grey),
                 ),
               ],
             ),
@@ -193,7 +167,7 @@ class _LoginPageState extends State<LoginPage> with WidgetsBindingObserver {
                             " ${account.cumtLoginLocation?.name} ${account.cumtLoginMethod?.name}",),),
                         IconButton(onPressed: (){
                           CumtLoginAccount.removeList(account);
-                          _showSnackBar(context, "删除成功");
+                          _showSnackBar("删除成功");
                           Navigator.of(context).pop();
                         }, icon: const Icon(Icons.close))
 
@@ -211,23 +185,18 @@ class _LoginPageState extends State<LoginPage> with WidgetsBindingObserver {
 
   void _handleLogout(BuildContext context) {
     CumtLogin.logout(account: cumtLoginAccount).then((value) {
-      _showSnackBar(context, value);
+      _showSnackBar(value);
     });
   }
 
-  void _showSnackBar(BuildContext context, String text) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(text),
-        duration: const Duration(seconds: 1),
-      ),
-    );
+  void _showSnackBar(String text) {
+    print(text);
   }
 
   void _handleLogin(BuildContext context) {
     if (_usernameController.text.trim().isEmpty ||
         _passwordController.text.trim().isEmpty) {
-      _showSnackBar(context, '账号或密码不能为空');
+      _showSnackBar('账号或密码不能为空');
       return;
     }
     cumtLoginAccount.username = _usernameController.text.trim();
@@ -235,7 +204,7 @@ class _LoginPageState extends State<LoginPage> with WidgetsBindingObserver {
 
     CumtLogin.login(account: cumtLoginAccount).then((value) {
       setState(() {
-        _showSnackBar(context, value);
+        _showSnackBar(value);
       });
     });
   }
