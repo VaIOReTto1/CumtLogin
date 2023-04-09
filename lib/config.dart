68959e36a9e920550fdd2ffa7bd076f88935009f
w6rsty <1486008923@qq.com>
2023-04-09 13:05:38 +0800

    feature(config.dart):添加了字体和布局配置
    添加了字体和布局配置，还未添加屏幕适配

---
+++ lib/config.dart
@@ -0,0 +1,26 @@
+class UIConfig {
+  // 屏幕大小
+  static double designWidth = 1440;
+  static double designHeight = 3120;
+
+  // 字体大小
+  static const double fontSizeTitle     = 34; // AppBar标题
+  static const double fontSizeSubTitle  = 30; // 关于我们、反馈、检查更新的标题
+  static const double fontSizeMain      = 28; // 主页面按钮 Drawer选项
+  static const double fontSizeSubMain   = 24; // 关于我们、反馈、检查更新正文
+  static const double fontSizeMid       = 22; // TextField 快捷入口
+  static const double fontSizeMin       = 20; // 校区&运营商
+
+  // 布局
+  static const double marginVertical    = 10; // 上下外边距
+  static const double marginHorizontal  = 15; // 左右外边距
+  static const double marginAll         = 13; // 所有外边距
+
+  static const double paddingVertical   = 12; // 上下内边距
+  static const double paddingHorizontal = 17; // 左右内边距
+  static const double paddingAll        = 15; // 所有内边距
+
+  static const double borderRadiusButton  = 18; // 按钮圆角
+  static const double borderRadiusEntry   = 10; // 快捷入口圆角
+  static const double borderRadiusBox     = 20; // 大块功能区圆角
+}
