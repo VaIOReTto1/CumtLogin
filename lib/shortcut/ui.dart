44ee4ec4941f58f3ebc920b937cca30d546209de
w6rsty <1486008923@qq.com>
2023-04-13 20:28:50 +0800

    feature(shortcut):完成逻辑，可以实现外跳转
    还未支持自定义网站和数据保存

--- lib/shortcut/ui.dart
+++ lib/shortcut/ui.dart
@@ -3,25 +3,30 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/services.dart';
 import '../config.dart';
 
-class Shortcut extends StatelessWidget {
+class Shortcut extends StatefulWidget {
   const Shortcut({Key? key}) : super(key: key);
 
   @override
+  State<Shortcut> createState() => _ShortcutState();
+}
+
+class _ShortcutState extends State<Shortcut> {
+  List<Map<String, String>> links = [
+    {"name" : "中国矿业大学", "url" : "https://www.cumt.edu.cn"},
+    {"name" : "必应", "url":"https://bing.com"},
+    {"name" : "物理实验中心", "url" : "http://10.103.4.10:8020/"},
+    {"name" : "教务选课", "url" : "http://10.0.2.2:8000"},
+    {"name" : "中国矿业大学馆藏数字化平台", "url" : "http://121.248.104.172:8080/"},
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(UIConfig.marginAll),
-      padding: EdgeInsets.all(UIConfig.paddingAll),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(UIConfig.borderRadiusBox),
-        border: Border.all(
-            color: Theme.of(context).colorScheme.primary == Colors.blue
-            ? const Color.fromRGBO(230, 231, 233, 1)
-            : Theme.of(context).colorScheme.primary
-        )
-      ),
+    print("build");
+    return _buildShorcutBox(
+      context: context,
       child: Column(
         children: [
+          // 标题
           Container(
               padding: EdgeInsets.all(UIConfig.paddingAll),
               child: Text("快捷入口",style: TextStyle(fontSize: UIConfig.fontSizeSubTitle),)
@@ -29,25 +34,55 @@ class Shortcut extends StatelessWidget {
           Wrap(
             spacing: 10,
             children: [
-              _buildEntry(context, "中国矿业大学", "https://www.cumt.edu.cn"),
-              _buildEntry(context, "百度", "https://www.baidu.com"),
-              _buildAddButton()
+              // 遍历List创建快捷方式
+              for(int i = 0; i < links.length; i++)
+                _buildEntry(context, links[i]),
+              // 添加新的快捷方式
+              //_buildAddButton(context),
             ],
           )
         ],
-      ),
+      )
     );
   }
 
-  Widget _buildEntry(BuildContext context, String name, String url) {
+  void _openUrl(String? url) {
+    launchUrl(
+        Uri.parse(url!),
+        mode: LaunchMode.externalApplication
+    );
+  }
+
+  Widget _buildShorcutBox({
+    required BuildContext context,
+    required Widget child
+  }) {
+    return Container(
+      margin: EdgeInsets.all(UIConfig.marginAll),
+      padding: EdgeInsets.all(UIConfig.paddingAll),
+      width: double.infinity,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(UIConfig.borderRadiusBox),
+          border: Border.all(
+              color: Theme.of(context).colorScheme.primary == Colors.blue
+                  ? const Color.fromRGBO(230, 231, 233, 1)
+                  : Theme.of(context).colorScheme.primary,
+              width: 2.0
+          )
+      ),
+      child: child,
+    );
+  }
+
+  Widget _buildEntry(BuildContext context, Map<String?, String?> web) {
     return InkWell(
       onLongPress: () {
-        Clipboard.setData(ClipboardData(text: url));
+        Clipboard.setData(ClipboardData(text: web["url"]));
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text("已复制到剪切板"))
         );
       },
-      onTap: () { launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication); },
+      onTap: () { _openUrl(web["url"]); },
       child: Container(
         margin: EdgeInsets.symmetric(vertical: UIConfig.marginVertical),
         padding: EdgeInsets.all(UIConfig.paddingAll),
@@ -63,16 +98,18 @@ class Shortcut extends StatelessWidget {
           alignment: WrapAlignment.center,
           children: [
             const Icon(Icons.link,),
-            Text(name, style: TextStyle(fontSize: UIConfig.fontSizeMid))
+            Text(web["name"]!, style: TextStyle(fontSize: UIConfig.fontSizeMid))
           ],
         ),
       ),
     );
   }
 
-  Widget _buildAddButton() {
+  Widget _buildAddButton(BuildContext context) {
     return InkWell(
-      onTap: () { print("Add new entry!");},
+      onTap: () {
+        setState(() {});
+      },
       child: Container(
         margin: EdgeInsets.symmetric(vertical: UIConfig.marginVertical),
         padding: EdgeInsets.all(UIConfig.paddingAll),
