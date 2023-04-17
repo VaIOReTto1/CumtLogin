ee12981f2d6a1d691acfca4ec618d3118551f64e
xiangxinliao43 <xiangxinliao43@gmail.com>
2023-04-17 23:06:48 +0800

    📑docs:统一windows的应用图标和程序名称

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,8 +26,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(450, 780);
-  if (!window.Create(L"cumt_login", origin, size)) {
+  Win32Window::Size size(500, 860);
+  if (!window.Create(L"校园网自动登录", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
