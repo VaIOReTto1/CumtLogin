0ba2700e7b0267697a169d9507931c3f77bc2147
xiangxinliao43 <xiangxinliao43@gmail.com>
2023-04-18 17:49:22 +0800

    📑docs:修改windows平台应用的图标和名称
    windows文件夹下的main.cpp文件不允许中文出现，所以暂时没法起中文名。

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(450, 780);
-  if (!window.Create(L"cumt_login", origin, size)) {
+  if (!window.Create(L"CumtLogin", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
